49602c3a492b3fff15edc16fb1b7e1de9addeaeb
BrianVelarde02 <A01753036@tec.mx>
2025-03-31 17:23:21 -0600

    correccion de un bug
    Error en el timer arreglado

--- Prueba Entregable/Assets/Scripts/QuizToF/QuizManagerBool.cs
+++ Prueba Entregable/Assets/Scripts/QuizToF/QuizManagerBool.cs
@@ -81,7 +81,8 @@ public class QuizManagerBool : MonoBehaviour
     }
 
     internal void TiempoAgotado()
-    {
+    {   
+        
         QnA.RemoveAt(currentQuestion);
         feedbackManager.RegistrarRespuesta(false);
         GeneraPregunta();
